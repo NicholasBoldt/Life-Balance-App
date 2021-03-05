b35a4475747f14895d6844a9d53b77bf3affda20
nicholasboldt <nicholas.boldt@gmail.com>
2021-03-05 14:54:56 -0800

    removed role component that had no purpose

--- src/pages/RoleDetailPage/RoleDetailPage.jsx
+++ src/pages/RoleDetailPage/RoleDetailPage.jsx
@@ -1,23 +1,25 @@
-import React, {useState, useEffect} from 'react'
-import Role from "../../components/Role/Role";
+import React, { useState, useEffect } from "react";
+
 import HabitForm from "../../components/HabitForm/HabitForm";
 import TaskForm from "../../components/TaskForm/TaskForm";
+import Habit from "../../components/Habit/Habit";
+import Task from "../../components/Task/Task";
 import "./RoleDetailPage.css";
 
 function RoleDetailPage(props) {
-    const [roleId, setRoleId] = useState(props.location.state?.role._id);
-    const [currentRole, setCurrentRole] = useState({});
+  const [roleId, setRoleId] = useState(props.location.state?.role._id);
+  const [currentRole, setCurrentRole] = useState({});
 
-    useEffect(() => { 
-        const roleIdx = props.roles.findIndex((role)=>{
-            return role._id == roleId
-        }) 
-        setCurrentRole(props.roles[roleIdx]);
-    }, [props])
+  useEffect(() => {
+    const roleIdx = props.roles.findIndex((role) => {
+      return role._id == roleId;
+    });
+    setCurrentRole(props.roles[roleIdx]);
+  }, [props]);
 
   return (
     <div className="RoleDetailPage">
-      <Role
+      {/* <Role
         currentRole={currentRole}
         name={currentRole.name}
         habits={currentRole.habits}
@@ -25,7 +27,36 @@ function RoleDetailPage(props) {
         handleDeleteTask={props.handleDeleteTask}
         handleCompleteHabit={props.handleCompleteHabit}
         handleGetStreak={props.handleGetStreak}
-      />
+      /> */}
+      <div>
+        <div className="header-footer">{currentRole.name}</div>
+        <h4> Habits </h4>
+        {currentRole.habits
+          ? currentRole.habits.map((habit) => (
+              <Habit
+                currentRole={currentRole}
+                id={habit._id}
+                currentHabit={habit}
+                name={habit.name}
+                amount={habit.amount}
+                completed={habit.completed}
+                handleCompleteHabit={props.handleCompleteHabit}
+                handleGetStreak={props.handleGetStreak}
+                displayEdit={true}
+              />
+            ))
+          : ""}
+        <h4> Tasks </h4>
+        {currentRole.tasks
+          ? currentRole.tasks.map((task) => (
+              <Task
+                name={task.name}
+                taskId={task._id}
+                handleDeleteTask={props.handleDeleteTask}
+              />
+            ))
+          : " "}
+      </div>
       <HabitForm
         role={currentRole}
         handleAddHabit={props.handleAddHabit}
