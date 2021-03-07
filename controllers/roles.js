8d50ff80f43d0d2e41e3b6f1d6aee58e711bcdd1
nicholasboldt <nicholas.boldt@gmail.com>
2021-03-07 13:53:39 -0800

    will now show the current streak that exists before a habit is completed

--- controllers/roles.js
+++ controllers/roles.js
@@ -143,6 +143,18 @@ async function addTask(req, res) {
             current_date.setDate(current_date.getDate() - 1)
         }
     }
+    let yesterdayDate = new Date()
+    yesterdayDate.setHours(0,0,0,0)
+    yesterdayDate.setDate(yesterdayDate.getDate() - 1)
+    if(streak === 0) {
+        for (let i = dates.length -1; i >= 0; i--) {
+            while(i >= 0 && dates[i].getTime() == yesterdayDate.getTime()) {
+                streak++
+                i--
+                yesterdayDate.setDate(yesterdayDate.getDate() - 1)
+            }
+        }
+    }
     console.log(streak)
     res.status(200).json(streak);
 }
